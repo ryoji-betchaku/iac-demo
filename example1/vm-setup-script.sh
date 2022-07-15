de139d526067b8faf982d452c78ea67759d561f9
Ryoji Betchaku <ryobet@Ryojis-MacBook-Pro.local>
2022-07-15 10:48:52 -0400

    adding files

--- example1/vm-setup-script.sh
+++ example1/vm-setup-script.sh
@@ -19,7 +19,3 @@ fi
 # Setup Lacework via Docker 
 sudo docker pull lacework/datacollector:latest
 sudo docker run -d --name datacollector --net=host --pid=host --privileged --volume /:/laceworkfim:ro --volume /var/lib/lacework:/var/lib/lacework --volume /var/log:/var/log --volume /var/run:/var/run --volume /etc/passwd:/etc/passwd:ro --volume /etc/group:/etc/group:ro --env ACCESS_TOKEN=${LACEWORK_ACCESS_TOKEN} lacework/datacollector:latest
-
-# sudo wget https://s3-us-west-2.amazonaws.com/www.lacework.net/download/5.1.0.6419_2021-12-18_release-v5.1_d0fd83c92c604ed47e8485222b0c42b4d5f20684/install.sh
-# sudo chmod +x install.sh
-# sudo ./install.sh
